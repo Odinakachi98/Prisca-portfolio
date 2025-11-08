d17bfe84c76bc8fc155893da2390aa4429ebc3b7
Odinakachi98 <linusodinakachi1998@gmail.com>
2025-11-08 08:32:29 +0100

    Made an update

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -20,7 +20,7 @@ const About = () => {
       <div className="about-right">
        <div className="about-para">
         <p>I am an passionate Frontend Developer with a works with details and a love for creating user experience with a clean code and beautiful design</p>
-        <p>I'm also a lover of knowledge,novelty and a good food. so, when I'm not codind, I'm reading, reseraching and experimenting in the kitchen. </p>
+        <p>I'm also a lover of knowledge,novelty and a good food. so, when I'm not coding, I'm reading, researching and experimenting in the kitchen. </p>
        </div>
 
        <div className="about-skills">
@@ -34,7 +34,7 @@ const About = () => {
      <div className="about-achievements">
 
       <div className="about-achievement">
-        <h1>2</h1>
+        <h1>4</h1>
         <p>YEARS OF EXPERIENCE</p>
       </div>
       <hr />
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
       <div className="footer-top">
         <div className="footer-top-left">
           {<img src={theme} alt="" style={{ width: "100px", height: "100px" }} /> }
-          <p>Liked what you're seeing? Let's talk about code and design.</p>
+          <p>Do you like what you're seeing? Then, let's talk about code and design.</p>
         </div>
         <div className="footer-top-right">
           <div className="footer-email-input">
